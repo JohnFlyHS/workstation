c5f17aa6b6996611117b612a3ec5a3d22bf59b08
johnflyhs <john.fly@healthstream.com>
2017-03-20 20:07:09 +0000

    iis content test

--- cookbooks/iis/test/smoke/default/default_test.rb
+++ cookbooks/iis/test/smoke/default/default_test.rb
@@ -1,18 +1,7 @@
-# # encoding: utf-8
-
-# Inspec test for recipe iis::default
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
+describe port(80) do
+  it(should_be_listening)
 end
 
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe command('(Invoke-WebRequest -UseBasicParsing localhost).Content') do
+  its(:stdout) { should match /Hello, world!/ }
 end
