ca9825caa9d153f7dbd21e798aeca00ce8c5a065
johnflyhs <john.fly@healthstream.com>
2017-03-20 15:58:42 +0000

    configure iis

---
+++ cookbooks/iis/recipes/server.rb
@@ -0,0 +1,13 @@
+powershell_script 'Install IIS' do
+  code 'Add-WindowsFeature Web-Server'
+  guard_interpreter :powershell_script
+  not_if "(Get-WindowsFeature -Name Web-Server).Installed"
+end
+
+file 'C:\inetpub\wwwroot\Default.htm' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'w3svc' do
+  action [:enable, :start]
+end
